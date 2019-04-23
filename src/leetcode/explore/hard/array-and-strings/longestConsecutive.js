aa5a699480fd0432189015e2f4095c1fce1743a4
cmmdev <cmmdev@qq.com>
2019-04-23 09:15:15 +0800

    longest consquence

--- src/leetcode/explore/hard/array-and-strings/longestConsecutive.js
+++ src/leetcode/explore/hard/array-and-strings/longestConsecutive.js
@@ -11,10 +11,43 @@
 解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
  */
 
- /**
- * @param {number[]} nums
- * @return {number}
- */
-var longestConsecutive = function(nums) {
-    
+/**
+* @param {number[]} nums
+* @return {number}
+*/
+var longestConsecutive = function (nums) {
+    const rangeMap = new Map()
+    nums.forEach(num => {
+        if (!rangeMap.get(num)) {
+            rangeMap.set(num, [num, num])
+        }
+    })
+    nums.forEach(num => {
+        if (rangeMap.get(num - 1) || rangeMap.get(num + 1)) {
+            if (rangeMap.get(num - 1)) {
+                let [min, max] = rangeMap.get(num)
+                rangeMap.set(num, rangeMap.get(num - 1))
+
+                rangeMap.get(num)[0] = Math.min(rangeMap.get(num)[0], min)
+                rangeMap.get(num)[1] = Math.max(rangeMap.get(num)[1], max)
+            }
+
+            if (rangeMap.get(num + 1)) {
+                let [min, max] = rangeMap.get(num)
+                rangeMap.set(num, rangeMap.get(num + 1))
+                rangeMap.get(num)[0] = Math.min(rangeMap.get(num)[0], min)
+                rangeMap.get(num)[1] = Math.max(rangeMap.get(num)[1], max)
+            }
+        }
+    })
+    let max = 0;
+    nums.forEach(num => {
+        let range = rangeMap.get(num)[1] - rangeMap.get(num)[0] + 1
+        if (range > max) {max = range}
+    })
+    console.log(rangeMap)
+    return max
 };
+
+// console.log(longestConsecutive([1, 2, 0, 1]))
+console.log(longestConsecutive([-4,-1,4,-5,1,-6,9,-6,0,2,2,7,0,9,-3,8,9,-2,-6,5,0,3,4,-2]))
