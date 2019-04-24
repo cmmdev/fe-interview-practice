2c889f20f64dbd87023993aa302c15b019043aa0
cmmdev <cmmdev@qq.com>
2019-04-24 09:36:11 +0800

    ..

---
+++ test/leetcode/explore/hard/array-and-strings/spiralOrder.spec.js
@@ -0,0 +1,16 @@
+var assert = require('assert')
+var { add } = require('../../src/add.js')
+
+describe('add without +/-', function() {
+    describe('test add', function() {
+        it('3+9 == 12', function() {
+            assert.equal(add(3, 9), 12)
+        });
+        it('3-9 == -6', function() {
+            assert.equal(add(3, -9), -6)
+        })
+        it('-3-9 == -12', function() {
+            assert.equal(add(-3, -9), -12)
+        })
+    })
+})
